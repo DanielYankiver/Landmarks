5ac406b55f8d0314f7d2253c54a12f39190ed131
Daniel Yankiver <dyankiver@gmail.com>
2024-01-05 14:10:22 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Daniel Yankiver on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
