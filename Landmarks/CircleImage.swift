10af0d6221d83c52e3b894c9b8e0a9b046bc3faf
Daniel Yankiver <dyankiver@gmail.com>
2024-01-08 10:51:44 -0500

    created a custom image view

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Daniel Yankiver on 1/8/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+        .clipShape(Circle())
+        .overlay {
+          Circle().stroke(.white, lineWidth: 4)
+        }
+        .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
