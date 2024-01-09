3e4c4f3e703082a0e6cda8e3387241f0d12bb4eb
Daniel Yankiver <dyankiver@gmail.com>
2024-01-09 16:11:44 -0500

    setup nav between list and detail views

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    var body: some View {
+  var body: some View {
+    NavigationSplitView {
       List(landmarks) { landmark in
-        LandmarkRow(landmark: landmark)
+        NavigationLink {
+          LandmarkDetail()
+        } label: {
+          LandmarkRow(landmark: landmark)
+        }
       }
+      .navigationTitle("Landmarks")
+    } detail: {
+      Text("Select a Landmark")
     }
+  }
 }
 
 #Preview {
-    LandmarkList()
+  LandmarkList()
 }
