9ea12dd15545b0e23ab8d82ca27ea49683d9f5bc
Daniel Yankiver <dyankiver@gmail.com>
2024-01-15 09:40:59 -0500

    created bottom portion of the symbol

--- Landmarks/BadgeSymbol.swift
+++ Landmarks/BadgeSymbol.swift
@@ -18,13 +18,21 @@ struct BadgeSymbol: View {
         let topWidth = width * 0.226
         let topHeight = height * 0.488
 
-
         path.addLines([
           CGPoint(x: middle, y: spacing),
           CGPoint(x: middle - topWidth, y: topHeight - spacing),
           CGPoint(x: middle, y: topHeight / 2 + spacing),
           CGPoint(x: middle + topWidth, y: topHeight - spacing),
           CGPoint(x: middle, y: spacing)
+        ])
+
+        path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+        path.addLines([
+          CGPoint(x: middle - topWidth, y: topHeight + spacing),
+          CGPoint(x: spacing, y: height - spacing),
+          CGPoint(x: width - spacing, y: height - spacing),
+          CGPoint(x: middle + topWidth, y: topHeight + spacing),
+          CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
         ])
       }
     }
