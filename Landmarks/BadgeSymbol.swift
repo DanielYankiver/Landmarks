d722281ee5fc4aae9f800b6022360e45cb022de0
Daniel Yankiver <dyankiver@gmail.com>
2024-01-15 09:39:05 -0500

    created new custom view called BadgeSymbol

---
+++ Landmarks/BadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Daniel Yankiver on 1/15/24.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  BadgeSymbol()
+}
